216b83e523e1baca751c7720d11d0f2c254f0433
guisousa <guisousa.dcc@gmail.com>
2013-06-01 14:22:44 -0300

    Using vim-pandemic instead of the ruby script to manage vim bundles.

--- setup.sh
+++ setup.sh
@@ -20,7 +20,6 @@ function error()
     echo $'\e[31m'"$1"
     tput sgr0
 }
-
 # Detecting System
 home='/home/'${USER}
 platform='unknown'
@@ -34,19 +33,52 @@ elif [[ "$unamestr" == 'Darwin' ]]; then
 fi
 
 # Helper functions
+function uninstall()
+{
+    rm -rf ${home}/.vim/pandemic-bundles
+    rm -rf ${home}/.vim/bundle.remote
+    for b in `pandemic list | column -t | cut -d ":" -f 1`; do
+        pandemic remove $b
+    done
+}
+function install()
+{
+    pandemic add xterm-color-table.vim  git  git://github.com/guns/xterm-color-table.vim.git
+    pandemic add tabular                git  git://github.com/godlygeek/tabular.git
+    pandemic add refactor               git  git://github.com/vim-scripts/refactor.git
+    pandemic add vim-visual-star-search git  git://github.com/nelstrom/vim-visual-star-search.git
+    pandemic add vim-unimpaired         git  git://github.com/tpope/vim-unimpaired.git
+    tmp=`mktemp -u /tmp/tmp.XXXXX` && ruby -ne 'puts chomp'  ~/.vim/bundle.remote/refactor/plugin/refactor.vim > $tmp && mv $tmp ~/.vim/bundle.remote/refactor/plugin/refactor.vim
+}
 function update()
 {
-    ~/.vim/update_bundles
-    tmp=`mktemp -u /tmp/tmp.XXXXX` && ruby -ne 'puts chomp'  ~/.vim/bundle/refactor/plugin/refactor.vim > $tmp && mv $tmp ~/.vim/bundle/refactor/plugin/refactor.vim
+    pandemic update
+    tmp=`mktemp -u /tmp/tmp.XXXXX` && ruby -ne 'puts chomp'  ~/.vim/bundle.remote/refactor/plugin/refactor.vim > $tmp && mv $tmp ~/.vim/bundle.remote/refactor/plugin/refactor.vim
 }
+
+# Install pandemic if necessary
+pandemic=`pandemic -h 2> /dev/null 1> /dev/null;echo $?`
+if [[ "$pandemic" -ne 0 ]]; then
+    git clone git://github.com/jwcxz/vim-pandemic.git
+    sudo python setup.py install
+fi
 
 # Copying files to right locations
 success "--- Copying files ---"
-cp -v ${home}/.vim/update_bundles .old_update_bundles
 cp -v ${home}/.vimrc .old_vimrc
-cp -v update_bundles ${home}/.vim/
 cp -v vimrc ${home}/.vimrc
-success "--- Updating Plugins ---"
-update
+if [[ "$1" == "-update" ]]; then
+    success "--- Updating Plugins ---"
+    update
+else
+    success "--- Uninstalling Plugins ---"
+    uninstall
+    success "--- Installing Plugins ---"
+    install
+fi
+if [[ ${platform} == 'linux' ]]; then
+    scp -r ${home}/.vimrc chaiten.jda.local:
+    scp -r ${home}/.vim   chaiten.jda.local:
+fi
 success "--- Setup Done ---"
 
